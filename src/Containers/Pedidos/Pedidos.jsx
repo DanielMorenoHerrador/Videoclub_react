7b6f88efdd7385f12259ab2d2ad67fd5d51fb15f
DanielMorenoHerrador <90707206+DanielMorenoHerrador@users.noreply.github.com>
2021-11-29 04:16:54 +0100

    Intentar mapear solo los pedidos realizados por el usuario logeado y mapear todos los pedidos en el endpoint todoslospedidos solo visible para el admin

--- src/Containers/Pedidos/Pedidos.jsx
+++ src/Containers/Pedidos/Pedidos.jsx
@@ -1,6 +1,7 @@
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import loading from '../../assets/img/loading.gif';
 
 
 const Pedidos = (props) => {
@@ -9,21 +10,52 @@ const Pedidos = (props) => {
     const [pedidos, setPedidos] = useState([]);
     const [msgError, setmsgError] = useState("");
 
+    useEffect(() => {
+
+        setTimeout(() => {
+
+            traerPedidos();
+        }, 2000);
+
+    }, []);
+
+    useEffect(() => {
+
+    });
+
     const traerPedidos = async () => {
         try {
-            let res = await axios.get("http://localhost:4000/pedidos");
+            let res = await axios.get("https://videoclub1.herokuapp.com/pedidos");
                 setPedidos(res.data);
         } catch (error) {
             setmsgError("No se ha podido traer los pedidos");
         }
     };
 
-    return (
+    if (pedidos[1]?.userId) {
+        return (
         <div className="designPedidos">
-            Estas en pedidos
-        </div>
-    )
+            {pedidos.map((pedido) => {
+                return (
+                    <div key={pedido.id} className="pedidosContainer">
+                        <h4>pedido Number: {pedido.id}</h4>
+                        <p>User Name: {pedido.user}</p>
+                        <p>User ID: {pedido.userId}</p>
+                        <p>Rented pelicula: {pedido.pelicula?.id}</p>
+                        <p>pelicula ID: {pedido.peliculaId}</p>
+                    </div>
+                );}
+        )
+        
+    } </div>
+        )}  else {
 
-}
+        return (
+            <div>
+                <img className="loader" src={loading} />
+            </div>
+        )
+    }}
+    
 
 export default Pedidos;
--- src/Containers/Todoslospedidos/Todoslospedidos.jsx
+++ src/Containers/Todoslospedidos/Todoslospedidos.jsx
@@ -1,21 +1,60 @@
-
-import React from 'react';
-
-const TodosLosPedidos = () => {
-
-
-    return (
-        <div>
-            Todos los pedidos
-        </div>
-
-    )
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import loading from '../../assets/img/loading.gif';
 
 
+const TodosLosPedidos = (props) => {
 
+    //HOOKS
+    const [pedidos, setPedidos] = useState([]);
+    const [msgError, setmsgError] = useState("");
 
+    useEffect(() => {
 
+        setTimeout(() => {
 
-}
+            traerPedidos();
+        }, 2000);
+
+    }, []);
+
+    useEffect(() => {
+
+    });
+
+    const traerPedidos = async () => {
+        try {
+            let res = await axios.get("https://videoclub1.herokuapp.com/pedidos");
+                setPedidos(res.data);
+        } catch (error) {
+            setmsgError("No se ha podido traer los pedidos");
+        }
+    };
+
+    if (pedidos[1]?.userId) {
+        return (
+        <div className="designPedidos">
+            {pedidos.map((pedido) => {
+                return (
+                    <div key={pedido.id} className="pedidosContainer">
+                        <h4>pedido Number: {pedido.id}</h4>
+                        <p>User Name: {pedido.user}</p>
+                        <p>User ID: {pedido.userId}</p>
+                        <p>Rented pelicula: {pedido.pelicula?.id}</p>
+                        <p>pelicula ID: {pedido.peliculaId}</p>
+                    </div>
+                );}
+        )
+        
+    } </div>
+        )}  else {
+
+        return (
+            <div>
+                <img className="loader" src={loading} />
+            </div>
+        )
+    }}
+    
 
 export default TodosLosPedidos;
